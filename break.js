2e1c73bebf8f8fdd0a8b62312a3581382178138f
aysha3f <tahiaysha220@gmail.com>
2025-03-22 04:05:08 +0600

    break

---
+++ break.js
@@ -0,0 +1,26 @@
+for(let i = 0; i < 150; i++){
+    console.log(i);
+    if(i >= 5 ){
+        break;
+    }
+}
+console.log('life after break up');
+
+// let n = 54;
+// while(n > 25){
+//     console.log(n);
+//     if(n < 50){
+//         break;
+//     }
+//     n--;
+// }
+
+
+let n = 54;
+while(n > 25){
+    if(n < 50){
+        break;
+    }   
+console.log(n);
+ n--;
+}
