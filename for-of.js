bd63f5e155b123937a90fd0411e36bcf949b1c5e
aysha3f <tahiaysha220@gmail.com>
2025-03-22 00:08:51 +0600

    while loop

--- for-of.js
+++ for-of.js
@@ -1,18 +1,18 @@
-const numbers = [12, 98, 45, 63, 21, 72, 53, 81, 56, 72,];
+// const numbers = [12, 98, 45, 63, 21, 72, 53, 81, 56, 72,];
 
-console.log(numbers);
+// console.log(numbers);
 
-// for( const num of numbers){
-//     console.log(num)
+// // for( const num of numbers){
+// //     console.log(num)
+// // }
+
+
+
+
+
+// const fruits = ['orange','apple', 'banana', 'jackfruit', 'watermelon' ]
+
+// for(const fruit of fruits){
+//     console.log(fruit);
+//     console.log('I want to eat');
 // }
-
-
-
-
-
-const fruits = ['orange','apple', 'banana', 'jackfruit', 'watermelon' ]
-
-for(const fruit of fruits){
-    console.log(fruit);
-    console.log('I want to eat');
-}
---
+++ while.js
@@ -0,0 +1,10 @@
+let num = 0;
+while(num <5){
+    console.log();
+    num = num + 1;
+}
+// // let num=0;
+// while(num<5){
+//     num++;//0=>1
+//     console.log(num)
+// }
