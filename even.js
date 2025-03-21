335e099d1ece5631b682ad63631f1416de5476bd
aysha3f <tahiaysha220@gmail.com>
2025-03-22 00:41:26 +0600

    even number

---
+++ even.js
@@ -0,0 +1,19 @@
+// // let num = 1;
+// // while(num <= 10){
+// //     console.log(num);
+
+// //     if(num % 2 === 0){
+// //         console.log('even number', num)
+// //     }
+
+// }
+let num = 1;
+while (num <= 10) {
+    console.log(num);
+
+    if (num % 2 === 0) {
+        console.log('even number', num);
+    }
+
+    num++; // Increment num to avoid infinite loop
+}
